be51c366c76f08685b130ad68fe79d9df092ed99
Hugo@M1Air <44889113+otaku312@users.noreply.github.com>
2022-05-01 11:47:55 +0800

    feat: move train step into single script

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import json
 import os
 import random
@@ -5,34 +6,62 @@ import random
 from src.utils import PROJECT_ROOT_PATH
 from models.predict import predict_intent, predict_ner
 
+intents_json = json.loads(open(os.path.join(PROJECT_ROOT_PATH, 'data/intents.json')).read())
 chatbot_name = "DEV"
 context = {}
-intents_json = json.loads(open(os.path.join(PROJECT_ROOT_PATH, 'data/intents.json')).read())
+scene_context = {}
+user = '123'
 
-def chatbot_response(sentence: str, userId='123', show_detail=False):
+def chatbot_response(sentence: str, userId, show_detail=False):
+  ner = predict_ner(sentence)
   predicted = predict_intent(sentence)
   response = "Sorry, I don't understand"
+  user_intent = None
+  
+  if show_detail:
+    print('predicted:', predicted)
   
   for result in predicted:
     tag = result['intent']
     intents = intents_json['intents']
     
+    previous_context = context[userId] if userId in context else '' 
     for i in intents:
       if i['tag'] == tag:
         if 'context_set' in i:
           if show_detail:
-            print('context:', i['context_set'])
-            
+            print('context:', i['context_set'], 'prvious:', previous_context)
+          
           context[userId] = i['context_set']
           
-        if not 'context_filter' in i or (userId in context and i['context_filter'] == context[userId]):
+
+        if not 'context_filter' in i or ('context_filter' in i and 'context_set' in i) or (userId in context and i['context_filter'] == context[userId]):
           if show_detail:
             print('tag:', i['tag'])
             
-          response = random.choice(i['responses'])          
+          response = random.choice(i['responses'])     
+          user_intent = i['tag'] 
+          
+        if 'context_filter' in i:
+          print('\n\n')
+          print('-'*25, 'NLP', '-'*25)
+          print('predicted user intent: ', user_intent)
+          
+          doc = ner
+          for ent in doc.ents:
+            if i['context_filter'] == 'bkd-query' and  ent.label_ == 'destination'.upper():
+                scene_context['destination'] = ent.text
+            elif i['context_filter'] == 'bkt-query':
+              if ent.label_ == 'depature'.upper():
+                scene_context['depature'] = ent.text
+              elif ent.label == 'return'.upper():
+                scene_context['return'] = ent.text
+              
+            print(ent, ent.text, ent.label_)
+          
           break
   
-  return response
+  return (response, user_intent)
 
 
 if __name__ == '__main__':
@@ -41,18 +70,13 @@ if __name__ == '__main__':
     user_response = input()
     if user_response in ['bye', 'exit', 'quit']:
       print(f'${chatbot_name}: bye')
+      print('\n')
+      print('-'*10, 'booking summary', '-'*10)
+      print(scene_context)
       break
     
-    response = chatbot_response(user_response)
-    ner = predict_ner(user_response)
+    response, user_intent = chatbot_response(user_response, userId=user, show_detail=True)
     
     print('-'*25, 'RESPONSE', '-'*25)
-    print(f'${chatbot_name}: ${response}')
-    print('-'*25, 'RESPONSE', '-'*25)
+    print(f'${chatbot_name}: ${response}\n')
     
-    print('\n\n')
-    print('-'*25, 'NLP', '-'*25)
-    doc = ner
-    for ent in doc.ents:
-      print(ent, ent.text, ent.label_)
-    print('-'*25, 'NLP', '-'*25)
---
+++ test/train_model.py
@@ -0,0 +1,28 @@
+from ast import Lambda, literal_eval
+import ast
+import os
+import pandas as pd
+import numpy as np
+import subprocess
+
+from src.models.train import train_chatbot_model, train_ner_model
+from src.data.generate_data import generate_ner_data, generate_chatbot_data
+from src.utils import PROJECT_ROOT_PATH
+
+
+df =  pd.read_csv(os.path.join(PROJECT_ROOT_PATH, 'data/chatbot-intents.csv'))
+if __name__ == '__main__':  
+  # pare string list to numeric list
+  df['pattern'] = df['pattern'].transform(lambda x: ast.literal_eval(x))
+  df['label'] = df['label'].transform(lambda x: ast.literal_eval(x))
+  
+  X = df['pattern'].tolist()
+  y = df['label'].tolist()
+  X = np.asanyarray(X).astype(np.int32)
+  y = np.asanyarray(y).astype(np.int32)
+  
+  train_chatbot_model(X, y)
+
+  
+  # excute spacy train ner script
+  subprocess.call(['sh', os.path.join(PROJECT_ROOT_PATH, 'src/models/ner/spacy-train.sh')])
